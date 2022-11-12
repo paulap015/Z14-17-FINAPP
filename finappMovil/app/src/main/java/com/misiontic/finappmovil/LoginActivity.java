825c66f868da9798e8af6e5ddfd78f268a76513c
paulapc <ppena@unicauca.edu.co>
2022-11-12 13:50:18 -0500

    first commmit login checked and return

--- finappMovil/app/src/main/java/com/misiontic/finappmovil/LoginActivity.java
+++ finappMovil/app/src/main/java/com/misiontic/finappmovil/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.misiontic.finappmovil;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,9 +27,18 @@ public class LoginActivity extends AppCompatActivity {
                 String valorUsuario = usuario.getText().toString();
                 String valorClave = usuario.getText().toString();
 
-                if(!valorUsuario.isEmpty() && !valorClave.isEmpty()){
+                if( (!valorUsuario.isEmpty() && !valorClave.isEmpty())){
                     Intent intent = new Intent(v.getContext(),MainActivity.class);
+                    intent.putExtra("usuario",valorUsuario);
                     startActivity(intent);
+                }else{
+                    /*AlertDialog.Builder mensaje = new AlertDialog.Builder(v.getContext());
+                    mensaje.setTitle("Campos vacios");
+                    mensaje.setMessage("Digite todos los campos");
+                    mensaje.show();*/
+
+                    Toast.makeText(v.getContext(), "Digite los campos requeridos. ",Toast.LENGTH_LONG).show();
+
                 }
             }
         });
--- finappMovil/app/src/main/java/com/misiontic/finappmovil/MainActivity.java
+++ finappMovil/app/src/main/java/com/misiontic/finappmovil/MainActivity.java
@@ -2,7 +2,14 @@ package com.misiontic.finappmovil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Bundle datos=getIntent().getExtras();
+        if(datos!=null){
+            TextView usuario = findViewById(R.id.main_txtUser);
+            usuario.setText(datos.getString("usuario"));
+        }
+
+        Button btnAbout = findViewById(R.id.btn_about);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://github.com/paulap015"));
+                startActivity(intent);
+            }
+        });
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton btnSalir = findViewById(R.id.main_btnSalir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed(); //devolverse
+            }
+        });
+
     }
 }
