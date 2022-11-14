820f2480ef49e1d8f90f3baa39db0b90f875b6da
paulapc <ppena@unicauca.edu.co>
2022-11-14 12:15:40 -0500

    added location permission

--- finappMovil/app/src/main/java/com/misiontic/finappmovil/MainActivity.java
+++ finappMovil/app/src/main/java/com/misiontic/finappmovil/MainActivity.java
@@ -1,27 +1,43 @@
 package com.misiontic.finappmovil;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-
-import com.misiontic.finappmovil.dialogs.MessageDialog;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int locationPermission = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnSucursales = findViewById(R.id.btnSucursales);
+        btnSucursales.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(MainActivity.this, "Ya se ha otrogado ete permiso", Toast.LENGTH_SHORT).show();
+                }else{
+                    requestLocationPermission();
+                }
+            }
+        });
 
         Bundle datos=getIntent().getExtras();
         if(datos!=null){
@@ -63,4 +79,36 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    private void requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            new AlertDialog.Builder(this).setTitle("Permiso").setMessage("Es necesario para mostrar sucursales cerca a tu ubicación").setPositiveButton("of", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},locationPermission);
+                }
+            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).create().show();
+        }else{
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},locationPermission);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == locationPermission) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(MainActivity.this, "Permiso garantizado", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Permiso NO otorgado", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 }
